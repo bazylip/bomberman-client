75c25ee383bf8e1bd96d91e33262d05c5a1999ed
Bazyli Polednia <bazyli.polednia@gmail.com>
2020-05-25 21:58:43 +0200

    Add basic rendering

--- bomberman_client/game/game.py
+++ bomberman_client/game/game.py
@@ -1,14 +1,16 @@
 import time
 
+import pygame
+
 from bomberman_client.game.game_mechanics import GameMechanics
-from bomberman_client.game.event_handler import EventHandler
 from bomberman_client.gui.game_interface import GameInterface
 from bomberman_client.player.player import Player
 
 
 class Game(GameMechanics, GameInterface):
     def __init__(self):
-        super().__init__()
+        GameMechanics.__init__(self)
+        GameInterface.__init__(self)
 
     def start_game(self):
         self.player = Player()
@@ -20,6 +22,7 @@ class Game(GameMechanics, GameInterface):
         self.player.get_initial_info()
         self.set_keys(self.player.id)
         while True:
+            pygame.event.pump()
             info = self.player.get_info_from_server()
             if info is not None:
                 if self.server_disconnected(info):
@@ -29,11 +32,10 @@ class Game(GameMechanics, GameInterface):
                     self.render_end_screen()
                     break
                 self.player.update_player_info(info)
-            self.render()
+            self.render(info)
             action = self.get_user_action()
             if action is not None:
                 self.player.send_action_to_server(action)
-            #time.sleep(1)
 
     def check_win_or_lose(self, info):
         return False
--- bomberman_client/gui/game_interface.py
+++ bomberman_client/gui/game_interface.py
@@ -1,8 +1,29 @@
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+import pygame
 
+
+BOARD_DIMENSION_X = 15
+BOARD_DIMENSION_Y = 9
+RESOLUTION_X = 800
+RESOLUTION_Y = 600
+GRASS_COLOR = (0,255,0)
+BLOCK_COLOR = (107,107,71)
+FIELD_SIZE_X = RESOLUTION_X / BOARD_DIMENSION_X
+FIELD_SIZE_Y = RESOLUTION_Y / BOARD_DIMENSION_Y
 
 class GameInterface:
-    def render(self):
-        pass
+    def __init__(self):
+        pygame.init()
+        self.display = pygame.display.set_mode(size=(RESOLUTION_X, RESOLUTION_Y))
+        pygame.display.set_caption("Bomberman")
+
+    def render(self, board_state):
+        self.display.fill(GRASS_COLOR)
+        for block_x in range(1, 15, 2):
+            for block_y in range(1, 9, 2):
+                pygame.draw.rect(self.display, BLOCK_COLOR, [block_x*FIELD_SIZE_X, block_y*FIELD_SIZE_Y, FIELD_SIZE_X, FIELD_SIZE_Y])
+        pygame.display.update()
 
     def render_end_screen(self):
         pass
